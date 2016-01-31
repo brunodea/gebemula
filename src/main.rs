d65f8f70e104737512e9134e2e28077163f7997d
Bruno Romero de Azevedo <brunodea@inf.ufsm.br>
2016-01-31 01:03:13 -0200

    Add basic loop for word reading.

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,30 @@ fn main() {
     let args: Vec<_> = env::args().collect();
     if args.len() == 2 {
         let mut boostrap_rom = File::open(&args[1]).unwrap();
-        let mut data = Vec::new();
+        let mut data: Vec<u8> = Vec::new();
         boostrap_rom.read_to_end(&mut data).unwrap();
 
         let mut cpu: Cpu = Cpu::default();
-        cpu.reg8(GenReg8::A);
-        cpu.reg16(GenReg16::AF);
         println!("{:?}", cpu);
-        //for word in data {
-        //    println!("{:x}", word);
-        //} 
+
+        let mut word = Vec::new();
+        let mut data_iter = data.iter();
+        let l4_fn = |opcode: &u8| -> u8 { opcode >> 4 };
+        let r4_fn = |opcode: &u8| -> u8 { opcode & 0x0f };
+
+        loop {
+            match data_iter.next() {
+                Some(opcode) => {
+                    word.push(opcode);
+                    //word.push(data_iter.next().unwrap());
+                    for w in &word {
+                        print!("{:x}", w);
+                    }
+                    println!("");
+                    word.clear();
+                },
+                None => { break },
+        }
     } else {
         println!("Invalid number of arguments.");
     }
