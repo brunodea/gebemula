261c564183d7de5bdd26f7749362ac4babe32141
Bruno Romero de Azevedo <brunodea@inf.ufsm.br>
2016-02-02 18:08:30 -0200

    Yet another complete change to opcodes.
    This time it is finer grained, so I believe there is a higher chance of
    working fairly well for this emulator. However, I think it is still
    quite open for re-structuring.

--- src/cpu/opcode.rs
+++ src/cpu/opcode.rs
@@ -4,163 +4,192 @@ use cpu::cpu::instruction::Instruction;
 //TODO verify if all opcodes are in the correct AddressingMode.
 
 #[derive(Debug)]
-pub enum AddressingMode {
-    Immediate,
-    ImmediateExt,
-    Relative,
-    Extended,
-    Register,
-    RegisterIndirect,
-    Implied, //no specific Addressing Mode: instruction uses only 1 byte.
-    NoAddr16, //for opcodes that don't rely on Addressing Mode (e.g. STOP)
-    
-    Invalid, //for opcodes not available (e.g. 0xCB by itself, 0xE3, 0xDB, 0xED, etc)
-}
-
-impl AddressingMode {
-    fn is_immediate(opcode: u8) -> bool {
-        let l4: u8 = opcode >> 4;
-        let r4: u8 = opcode & 0x0F;
-
-        //from 8-bit Arithmetic and Logic
-        (l4 >= 0xC && (r4 == 0xE || r4 == 0x6)) ||
-        //GB Exclusive
-        (l4 <= 0x3 && (r4 == 0x6 || r4 == 0xE)) ||
-        (l4 >= 0xE && (r4 == 0x0 || r4 == 0x2)) ||
-        opcode == 0xF8
-    }
-    fn is_immediate_ext(opcode: u8) -> bool {
-        let l4: u8 = opcode >> 4;
-        let r4: u8 = opcode & 0x0F;
-        //from LD 16-bit
-        (l4 <= 0x3 && r4 == 0x1) ||
-        //from Jump,Call and Return
-        opcode == 0xC3 || opcode == 0xDA || opcode == 0xD2 || opcode == 0xCA || opcode == 0xC2 ||
-        opcode == 0xCD || opcode == 0xDC || opcode == 0xD4 || opcode == 0xCC || opcode == 0xC4 ||
-        //GB Exclusive
-        (l4 >= 0xE && r4 == 0xA)
-    }
-    fn is_relative(opcode: u8) -> bool {
-        //from Jump,Call and Return
-        opcode == 0x18 || opcode == 0x38 || opcode == 0x30 || opcode == 0x28 || opcode == 0x20
-    }
-    fn is_extended(opcode: u8) -> bool {
-        //GB Exclusive
-        opcode == 0x08
-    }
-    fn is_register(opcode: u8) -> bool {
-        let l4: u8 = opcode >> 4;
-        let r4: u8 = opcode & 0x0F;
-
-        //from LD 8-bit
-        (l4 >= 0x4 && l4 <= 0x6 && r4 != 0xE && r4 != 0x6) ||
-        (l4 == 0x7 && r4 >= 0x8 && r4 != 0xE) || 
-        //from LD 16-bit
-        opcode == 0xF9 ||
-        //from 8-bit Arithmetic and Logic
-        (l4 >= 0x8 && l4 <= 0xB && r4 != 0x6 && r4 != 0xE) ||
-        (l4 <= 0x2 && (r4 == 0xC || r4 == 0xD || r4 == 0x4 || r4 == 0x5)) ||
-        opcode == 0x3C || opcode == 0x3D ||
-        //GB Exclusive
-        opcode == 0xE8
-    }
-    fn is_register_indirect(opcode: u8) -> bool {
-        let l4: u8 = opcode >> 4;
-        let r4: u8 = opcode & 0x0F;
-
-        //from LD 8-bit
-        (l4 >= 0x4 && l4 <= 0x7 && (r4 == 0xE || r4 == 0x6)) || (opcode >= 0x70 && opcode <= 0x77) ||
-        opcode == 0x02 || opcode == 0x12 ||
-        //from LD 16-bit
-        (l4 >= 0xC && (r4 == 0x1 || r4 == 0x5)) ||
-        //from 8-bit Arithmetic and Logic
-        ((l4 >= 0x8 && l4 <= 0xB) && (r4 == 0x6 || r4 == 0xE)) ||
-        opcode == 0x34 || opcode == 0x35 || opcode == 0x74 || 
-        //from Jump,Call and Return
-        opcode == 0xC9 || opcode == 0xD8 || opcode == 0xD0 || opcode == 0xC8 || opcode == 0xC0 ||
-        opcode == 0xE9 ||
-        //GB Exclusive
-        (l4 <= 0x3 && (r4 == 0x2 || r4 == 0xA)) ||
-        opcode == 0x32 || opcode == 0xD9
-    }
-    fn is_implied(opcode: u8) -> bool {
-        let l4: u8 = opcode >> 4;
-        let r4: u8 = opcode & 0x0F;
-        //from General-Purpose AF Operation
-        opcode == 0x27 || opcode == 0x2F || opcode == 0x3F || opcode == 0x37 ||
-        //from Bit Arithmetic (INC and DEC)
-        (l4 <= 0x3 && (r4 == 0x3 || r4 == 0xB)) ||
-        //from Jump,Call and Return
-        (l4 >= 0xC && (r4 == 0x7 || r4 == 0xF)) ||
-        //from Miscellaneous CPU Control
-        opcode == 0x00 || opcode == 0xF3 || opcode == 0xFB ||
-        //from Rotate and Shift
-        (l4 <= 0x1 && (r4 == 0x7 || r4 == 0xF)) ||
-        //from 16-bit Arithmetic
-        (l4 <= 0x3 && r4 == 0x9)
-    }
-    fn is_noaddr16(opcode: u8) -> bool {
-        //GB Exclusive
-        opcode == 0x10
-    }
-
-    pub fn from_opcode(prefix: u8, opcode: u8) -> AddressingMode {
-        //let l4: u8 = opcode >> 4;
-        let r4: u8 = opcode & 0x0F;
-        if prefix == 0xCB {
-            //Rotate and Shifts
-            if r4 != 0x6 && r4 != 0xE {
-                return AddressingMode::Register
-            } else {
-                return AddressingMode::RegisterIndirect
-            }
-        } else {
-            if AddressingMode::is_immediate(opcode) {
-                return AddressingMode::Immediate
-            } else if AddressingMode::is_immediate_ext(opcode) {
-                return AddressingMode::ImmediateExt
-            } else if AddressingMode::is_relative(opcode) {
-                return AddressingMode::Relative
-            } else if AddressingMode::is_extended(opcode) {
-                return AddressingMode::Extended
-            } else if AddressingMode::is_register(opcode) {
-                return AddressingMode::Register
-            } else if AddressingMode::is_register_indirect(opcode) {
-                return AddressingMode::RegisterIndirect
-            } else if AddressingMode::is_implied(opcode) {
-                return AddressingMode::Implied
-            } else if AddressingMode::is_noaddr16(opcode) {
-                return AddressingMode::NoAddr16
-            } else {
-                return AddressingMode::Invalid
-            }
-        }
-        panic!("No addressing mode for opcode 0x{} and prefix 0x{}", format!("{:01$x}", opcode, 2), format!("{:01$x}", prefix, 2));
-    }
-}
-
-#[derive(Debug)]
 pub struct Opcode {
     pub opcode: u8,
-    pub prefix: u8,
-    pub num_bytes: u8,
     pub cycles: u8,
+    pub num_bytes: u8,
 }
 
 impl Opcode {
-    pub fn new(prefix: u8, opcode: u8, cycles: u8) -> Opcode {
-        let mut nb = 0;
-        if prefix == 0xCB {
-            nb = 2;
-        } else {
-            nb = OpcodeMap::opcode_num_bytes(AddressingMode::from_opcode(prefix, opcode));
-        }
+    pub fn new(opcode: u8, cycles: u8, num_bytes: u8) -> Opcode {
         Opcode {
             opcode: opcode,
-            prefix: prefix,
-            num_bytes: nb,
             cycles: cycles,
+            num_bytes: num_bytes,
         }
+    }
+
+    // TODO: Verify if these functions can be forced to be inlined with Rust.
+    
+    // checks if the last 3 bytes of bytes represent a register 8 (A,B,C,D,E,H,L)
+    fn is_reg8(bytes: u8) -> bool { (bytes & 0b111) != 0b110  }
+
+    fn is_prefix(opcode: u8) -> bool {
+        opcode == 0xCB
+    }
+
+    /* 8-Bit Load Group */
+    fn is_ld_r_r(opcode: u8) -> bool {
+        opcode >> 6 == 0b01 && Opcode::is_reg8(opcode >> 3) && Opcode::is_reg8(opcode)
+    }
+    fn is_ld_r_n(opcode: u8) -> bool {
+        opcode >> 6 == 0b00 && Opcode::is_reg8(opcode >> 3) && !Opcode::is_reg8(opcode)
+    }
+    fn is_ld_r_hl(opcode: u8) -> bool {
+        opcode >> 6 == 0b01 && Opcode::is_reg8(opcode >> 3) && !Opcode::is_reg8(opcode)
+    }
+    fn is_ld_hl_r(opcode: u8) -> bool {
+        opcode >> 6 == 0b01 && !Opcode::is_reg8(opcode >> 3) && Opcode::is_reg8(opcode)
+    }
+    fn is_ld_hl_n(opcode: u8) -> bool { opcode == 0x36 }
+    fn is_ld_a_bc(opcode: u8) -> bool { opcode == 0x0A }
+    fn is_ld_a_de(opcode: u8) -> bool { opcode == 0x1A }
+    fn is_ld_a_nn(opcode: u8) -> bool { opcode == 0x3A }
+    fn is_ld_bc_a(opcode: u8) -> bool { opcode == 0x02 }
+    fn is_ld_de_a(opcode: u8) -> bool { opcode == 0x12 }
+
+    /* 16-Bit Load Group */
+    fn is_ld_dd_nn(opcode: u8) -> bool {
+        opcode >> 6 == 0b00 && (opcode & 0b1) == 0b1
+    }
+    fn is_ld_hl_nn(opcode: u8) -> bool { opcode == 0x2A }
+    fn is_ld_nn_hl(opcode: u8) -> bool { opcode == 0x22 }
+    fn is_ld_sp_hl(opcode: u8) -> bool { opcode == 0xF9 }
+    fn is_push_qq(opcode: u8) -> bool { 
+        opcode >> 6 == 0b11 && (opcode & 0b1111) == 0b0101
+    }
+    fn is_pop_qq(opcode: u8) -> bool {
+        opcode >> 6 == 0b11 && (opcode & 0b1111) == 0b0001
+    }
+
+    /* 8-Bit Arithmetic Group */
+    fn is_add_a_r(opcode: u8) -> bool {
+        opcode >> 3 == 0b10000
+    }
+    fn is_add_a_n(opcode: u8) -> bool { opcode == 0xC6 }
+    fn is_adc_a_r(opcode: u8) -> bool { opcode >> 3 == 0b10001 }
+    fn is_adc_a_n(opcode: u8) -> bool { opcode == 0xCE }
+
+    fn is_sub_r(opcode: u8) -> bool { opcode >> 3 == 0b10010 }
+    fn is_sub_n(opcode: u8) -> bool { opcode == 0xD6 }
+
+    fn is_sbc_a_r(opcode: u8) -> bool { opcode >> 3 == 0b10011 }
+    fn is_sbc_a_n(opcode: u8) -> bool { opcode == 0xDE }
+
+    fn is_and_r(opcode: u8) -> bool { opcode >> 3 == 0b10100 }
+    fn is_and_n(opcode: u8) -> bool { opcode == 0xE6 }
+
+    fn is_or_r(opcode: u8) -> bool { opcode >> 3 == 0b10110 }
+    fn is_or_n(opcode: u8) -> bool { opcode == 0xF6 }
+    
+    fn is_xor_r(opcode: u8) -> bool { opcode >> 3 == 0b10101 }
+    fn is_xor_n(opcode: u8) -> bool { opcode == 0xEE }
+
+    fn is_cp_r(opcode: u8) -> bool { opcode >> 3 == 0b10111 }
+    fn is_cp_n(opcode: u8) -> bool { opcode == 0xFE }
+
+    fn is_inc_s(opcode: u8) -> bool {
+        opcode >> 6 == 0b0 && opcode & 0b111 == 0b100
+    }
+    fn is_dec_s(opcode: u8) -> bool {
+        opcode >> 6 == 0b0 && opcode & 0b111 == 0b101
+    }
+    
+    /* General-Purpose Arithmetic and CPU Control Groups */
+    fn is_daa(opcode: u8) -> bool { opcode == 0x27 }
+    fn is_cpl(opcode: u8) -> bool { opcode == 0x2F }
+    fn is_ccf(opcode: u8) -> bool { opcode == 0x3F }
+    fn is_scf(opcode: u8) -> bool { opcode == 0x37 }
+    fn is_nop(opcode: u8) -> bool { opcode == 0x00 }
+    fn is_halt(opcode: u8) -> bool { opcode == 0x76 }
+    fn is_di(opcode: u8) -> bool { opcode == 0xF3 }
+    fn is_ei(opcode: u8) -> bool { opcode == 0xFB }
+
+    /* 16-Bit Arithmetic Group */
+    fn is_add_hl_ss(opcode: u8) -> bool {
+        opcode >> 6 == 0b00 && opcode & 0b1111 == 0b1001
+    }
+    fn is_inc_ss(opcode: u8) -> bool {
+        opcode >> 6 == 0b00 && opcode & 0b1111 == 0b0011
+    }
+    fn is_dec_ss(opcode: u8) -> bool {
+        opcode >> 6 == 0b00 && opcode & 0b1111 == 0b1011
+    }
+
+    /* Rotate and Shift Group */
+    fn is_rlca(opcode: u8) -> bool { opcode == 0x07 }
+    fn is_rla(opcode: u8) -> bool { opcode == 0x17 }
+    fn is_rrca(opcode: u8) -> bool { opcode == 0x0F }
+    fn is_rra(opcode: u8) -> bool { opcode == 0x1F }
+
+    /* Jump Group */
+    fn is_jp_nn(opcode: u8) -> bool { opcode == 0xC3 }
+    fn is_jp_cc_nn(opcode: u8) -> bool {
+        opcode >> 6 == 0b11 && opcode & 0b111 == 0b010
+    }
+    fn is_jr_e(opcode: u8) -> bool { opcode == 0x18 }
+    fn is_jr_c_e(opcode: u8)-> bool { opcode == 0x38 }
+    fn is_jr_nc_e(opcode: u8) -> bool { opcode == 0x30 }
+    fn is_jr_z_e(opcode: u8) -> bool { opcode == 0x28 }
+    fn is_jr_nz_e(opcode: u8) -> bool { opcode == 0x20 }
+    fn is_jp_hl(opcode: u8) -> bool { opcode == 0xE9 }
+
+    /* Call and Return Group */
+    fn is_call_nn(opcode: u8) -> bool { opcode == 0xCD }
+    fn is_call_cc_nn(opcode: u8) -> bool {
+        opcode >> 6 == 0b11 && opcode & 0b111 == 0b100
+    }
+    fn is_ret(opcode: u8) -> bool { opcode == 0xC9 }
+    fn is_ret_cc(opcode: u8) -> bool {
+        opcode >> 6 == 0b11 && opcode & 0b111 == 0b000
+    }
+    fn is_rst_p(opcode: u8) -> bool {
+        opcode >> 6 == 0b11 && opcode & 0b111 == 0b111
+    }
+    
+    /* GB only */
+    fn is_reti(opcode: u8) -> bool { opcode == 0xD9 }
+    fn is_add_sp_n(opcode: u8) -> bool { opcode == 0xE8 }
+    fn is_ldi_hl_a(opcode: u8) -> bool { opcode == 0x22 }
+    fn is_ldd_hl_a(opcode: u8) -> bool { opcode == 0x32 } 
+    fn is_ldi_a_hl(opcode: u8) -> bool { opcode == 0x2A }
+    fn is_ldd_a_hl(opcode: u8) -> bool { opcode == 0x3A }
+    fn is_ldh_n_a(opcode: u8) -> bool { opcode == 0xE0 }
+    fn is_ldh_a_n(opcode: u8) -> bool { opcode == 0xF0 }
+    fn is_ldhl_sp_n(opcode: u8) -> bool { opcode == 0xF8 }
+    fn is_ld_nn_sp(opcode: u8) -> bool { opcode == 0x08 }
+    fn is_stop(opcode: u8) -> bool { opcode == 0x10 }
+    fn is_swap_n(opcode: u8) -> bool { opcode >> 3 == 0b00110 }
+
+    /* CB-Prefixed */
+    fn is_cb_rlc_s(opcode: u8) -> bool { opcode >> 3 == 0b00000 }
+    fn is_cb_rrc_s(opcode: u8) -> bool { opcode >> 3 == 0b00001 }
+    fn is_cb_rl_s(opcode: u8) -> bool { opcode >> 3 == 0b00010 }
+    fn is_cb_rr_s(opcode: u8) -> bool { opcode >> 3 == 0b00011 }
+    fn is_cb_sla_s(opcode: u8) -> bool { opcode >> 3 == 0b00100 }
+    fn is_cb_sra_s(opcode: u8) -> bool { opcode >> 3 == 0b00101 }
+    fn is_cb_srl_s(opcode: u8) -> bool { opcode >> 3 == 0b00111 }
+    fn is_cb_bit_s(opcode: u8) -> bool { opcode >> 6 == 0b01 }
+    fn is_cb_res_s(opcode: u8) -> bool { opcode >> 6 == 0b10 }
+    fn is_cb_set_s(opcode: u8) -> bool { opcode >> 6 == 0b11 }
+
+    //TODO functions like: fn opcode_reg8_lhs opcode_reg8_rhs opcode_reg16_lhs opcode_reg16_rhs
+    
+    //All opcodes are either 1, 2 or 3 bytes long.
+    fn is_2bytes_long(opcode: u8) -> bool {
+        Opcode::is_ld_r_n(opcode)     ||  Opcode::is_ld_hl_n(opcode)  ||  Opcode::is_add_a_n(opcode)  ||
+        Opcode::is_adc_a_n(opcode)    ||  Opcode::is_sub_n(opcode)    ||  Opcode::is_sbc_a_n(opcode)  ||
+        Opcode::is_and_n(opcode)      ||  Opcode::is_or_n(opcode)     ||  Opcode::is_xor_n(opcode)    ||
+        Opcode::is_cp_n(opcode)       ||  Opcode::is_jr_e(opcode)     ||  Opcode::is_jr_c_e(opcode)   ||
+        Opcode::is_jr_nc_e(opcode)    ||  Opcode::is_jr_z_e(opcode)   ||  Opcode::is_jr_nz_e(opcode)  ||
+        Opcode::is_add_sp_n(opcode)   ||  Opcode::is_ldh_n_a(opcode)  ||  Opcode::is_ldh_a_n(opcode)  ||
+        Opcode::is_ldhl_sp_n(opcode)  ||  Opcode::is_stop(opcode)
+    }
+    fn is_3bytes_long(opcode: u8) -> bool {
+        Opcode::is_ld_a_nn(opcode)   ||  Opcode::is_ld_dd_nn(opcode)  ||
+        Opcode::is_ld_hl_nn(opcode)  ||  Opcode::is_ld_nn_hl(opcode)  ||  Opcode::is_jp_nn(opcode)       ||
+        Opcode::is_jp_cc_nn(opcode)  ||  Opcode::is_call_nn(opcode)   ||  Opcode::is_call_cc_nn(opcode)  ||
+        Opcode::is_ld_nn_sp(opcode)
     }
 }
 
@@ -174,99 +203,22 @@ impl OpcodeMap {
     pub fn new() -> OpcodeMap {
         let mut op_map: HashMap<u16, Opcode> = HashMap::new();
         for opcode in 0x0..0xFF {
-            let cycles: u8 = OpcodeMap::opcode_cycles(opcode, false);
-            let opcode_obj: Opcode = Opcode::new(0x0, opcode, cycles);
+            let mut num_bytes: u8 = 2;
+            if opcode != 0xCB {
+                num_bytes = 1;
+                if Opcode::is_2bytes_long(opcode) {
+                    num_bytes = 2;
+                } else if Opcode::is_3bytes_long(opcode) {
+                    num_bytes = 3;
+                }
+            } else {
+                num_bytes = 0;
+            }
+            let opcode_obj: Opcode = Opcode::new(opcode, 0, num_bytes);
             op_map.insert(opcode as u16, opcode_obj);
-        }
-        //for cb-prefixed code.
-        for opcode in 0x0..0xFF {
-            let cycles: u8 = OpcodeMap::opcode_cycles(opcode, true);
-            let opcode_obj: Opcode = Opcode::new(0xCB, opcode, cycles);
-            let key: u16 = 0xCB00 | opcode as u16;
-            op_map.insert(key, opcode_obj);
         }
         OpcodeMap {
             map: op_map,
-        }
-    }
-
-    fn opcode_num_bytes(addr_mode: AddressingMode) -> u8 {
-        match addr_mode {
-            AddressingMode::Register         |
-            AddressingMode::RegisterIndirect |
-            AddressingMode::Implied => 1,
-
-            AddressingMode::Immediate |
-            AddressingMode::Relative  |
-            AddressingMode::NoAddr16 => 2,
-
-            AddressingMode::ImmediateExt |
-            AddressingMode::Extended => 3,
-
-            AddressingMode::Invalid => 0,
-        }
-    }
-
-    //*CAREFUL* -> some values from http://www.pastraiser.com/cpu/gameboy/gameboy_opcodes.html
-    //are different than in the gameboy manual. Preference to the manual.
-    fn opcode_cycles(opcode: u8, is_bit_op: bool) -> u8 {
-        let l4: u8 = opcode >> 4;
-        let r4: u8 = opcode & 0x0F;
-
-        if is_bit_op {
-            if r4 == 0x6 || r4 == 0xE {
-                return 16 as u8
-            } else {
-                return 8 as u8 
-            }
-        }
-
-        //all cases for cycle 8
-        if (r4 == 0x0 && (l4 == 0x3 || l4 == 0x2 || l4 == 0x7 || l4 == 0xC || l4 == 0xD)) ||
-           (r4 == 0x1 && l4 == 0x7) ||
-           (r4 == 0x2 && (l4 <= 0x3 || l4 == 0x7 || l4 >= 0xE)) ||
-           (r4 == 0x3 && (l4 <= 0x3 || l4 == 0x7)) ||
-           (r4 == 0x4 && l4 == 0x7) ||
-           (r4 == 0x5 && l4 == 0x7) ||
-           (r4 == 0x6 && l4 != 0x3 && l4 != 0x7) ||
-           (r4 == 0x7 && l4 == 0x7) ||
-           (r4 == 0x8 && (l4 == 0x2 || l4 == 0x3 || l4 == 0xC || l4 == 0xD || l4 == 0x1)) ||
-           (r4 == 0x9 && (l4 <= 0x3 || l4 == 0xF || l4 == 0xC || l4 == 0xD)) ||
-           (r4 == 0xA && l4 <= 0x3) ||
-           (r4 == 0xB && l4 <= 0x3) ||
-           (r4 == 0xE) {
-           
-           8 as u8
-        }
-        //all cases for cycle 12
-        else if opcode == 0xE0 || opcode == 0xF0 ||
-            (r4 == 0x1 && (l4 <= 0x3 || l4 >= 0xC)) ||
-            opcode == 0xC2 || opcode == 0xD2 ||
-            opcode == 0xC4 || opcode == 0xD4 ||
-            opcode == 0xF8 ||
-            opcode == 0xCA || opcode == 0xDA ||
-            opcode == 0xCC || opcode == 0xDC ||
-            opcode == 0xCD {
-
-            12 as u8
-        }
-        //all cases for cycle 16
-        else if opcode == 0xC3  ||
-            (r4 == 0x5 && l4 >= 0xC) ||
-            opcode == 0xE8 || 
-            opcode == 0xFA || opcode == 0xEA {
-            
-            16 as u8
-        } else if opcode == 0x08 {
-
-            20 as u8
-        } else if (r4 == 0x7 && l4 >= 0xC) ||
-            (r4 == 0xF && l4 >= 0xC) {
-
-            32 as u8
-        } else {
-
-            4 as u8
         }
     }
 
@@ -287,9 +239,6 @@ impl OpcodeMap {
                     let mut nbytes = opcode_obj.num_bytes;
                     if *opcode_byte == 0xCB {
                         nbytes = 2; //1 for supporting the CB prefix + 1 for the CB-prefixed instruction.
-                    } else if nbytes == 0 {
-                        println!("While fetching instructions, found invalid with 0 bytes: {:01$x}", *opcode_byte, 2);
-                        continue;
                     }
 
                     let mut instruction: Instruction = vec![0; nbytes as usize];
