d5ec1e29175f4000e1a230d6afc5ed86ed0474ad
Bruno Romero de Azevedo <brunodea@inf.ufsm.br>
2016-02-05 00:25:05 -0200

    Add util module.

---
+++ src/util/util.rs
@@ -0,0 +1,24 @@
+
+//TODO make sure both has_carry and has_borrow are correct implemented.
+
+pub fn has_carry_on_bit(bit: u8, byte_1: u8, byte_2: u8) -> bool {
+    let mask: u8 = 1 << bit;
+    byte_1 & mask == byte_2 & mask
+}
+
+pub fn has_borrow_on_bit(bit: u8, byte_1: u8, byte_2: u8) -> bool {
+    let mask: u8 = 1 << bit;
+    byte_1 & mask == 0b0 && byte_2 & mask == 0b1
+}
+
+pub fn has_borrow_on_any(byte_1: u8, byte_2: u8) -> bool {
+    let mut has = false;
+    for i in 0..8 {
+        has = has_borrow_on_bit(i, byte_1, byte_2);
+        if has {
+            break;
+        }
+    }
+
+    has
+}
