1d913a71883311aebc0d0a0ffe7c7b3c4b3af84a
Bruno Romero de Azevedo <brunodea@inf.ufsm.br>
2016-02-14 15:35:45 -0200

    Base timing on timeline.

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,3 +1,2 @@
 pub mod cpu;
 pub mod timer;
-pub mod interrupt;
--- src/gebemula.rs
+++ src/gebemula.rs
@@ -1,12 +1,14 @@
 use cpu::cpu::Cpu;
 use mem::mem::Memory;
-use cpu::interrupt::{Interrupt, InterruptType};
-use cpu::timer::Timer;
+use time::{Duration, PreciseTime};
+use std::time;
+use cpu::timer::{EventType, Event, Timeline};
+use std::thread;
 
 pub struct Gebemula {
     cpu: Cpu,
     mem: Memory,
-    timer: Timer,
+    timeline: Timeline,
 }
 
 impl Gebemula {
@@ -14,7 +16,7 @@ impl Gebemula {
         Gebemula {
             cpu: Cpu::new(),
             mem: Memory::new(),
-            timer: Timer::new(),
+            timeline: Timeline::new(),
         }
     }
 
@@ -26,32 +28,42 @@ impl Gebemula {
         self.mem.load_game_rom(game_rom);
     }
 
-    pub fn run(&mut self) {
+    fn init(&mut self) {
         self.cpu.reset_registers();
         self.mem.write_byte(0xFF44, 0x90); //for bypassing 'waiting for screen frame'.
 
-        let v_blank: &mut Interrupt = &mut Interrupt::new(InterruptType::V_BLANK);
+        //periodic events should be put in the queue beforehand.
+        self.timeline.push_event(Event::new(EventType::INTERRUPT_V_BLANK));
+    }
+
+    pub fn run(&mut self) {
+        self.init();
         loop {
-            if v_blank.interrupt() {
-                self.timer.push_event(v_blank.clone());
-            }
+            let start: PreciseTime = PreciseTime::now();
+            let max_cycles: u64 = self.timeline.cycles_until_next_event();
 
-            let max_cycles: u64 = match self.timer.cycles_until_next_event() {
-                Some(cycles) => cycles,
-                None => 0,
-            };
-
-            //only will work if there is at least one event in the queue.
             let mut cycles: u64 = 0;
             while cycles < max_cycles {
                 cycles += self.cpu.run_instruction(&mut self.mem) as u64;
             }
-            
+
             //next_events removes the event from the queue
-            if let Some(interrupt) = self.timer.next_event() {
-                //TODO
+            if let Some(event) = self.timeline.next_event() {
+                //TODO event code
+
+                //sleeps for the amount of time needed until the event actually
+                //should happen.
+                let duration: Duration = start.to(PreciseTime::now());
+                if let Some(offset_time) = event.offset_time(duration) {
+                    thread::sleep(time::Duration::new(0, offset_time as u32));
+                }
+                //put periodic events in the queue again.
+                match event.event_type {
+                    EventType::INTERRUPT_V_BLANK => {
+                        self.timeline.push_event(event);
+                    },
+                }
             }
-            //TODO sleep until next event time is reached.
         }
     }
 }
