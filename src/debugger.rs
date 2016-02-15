848779b2341918df6fbd04662bf77f504c2ddeeb
Bruno Romero de Azevedo <brunodea@inf.ufsm.br>
2016-02-15 18:45:03 -0200

    Add option to print cpu state/instruction during a break command.

--- src/debugger.rs
+++ src/debugger.rs
@@ -8,6 +8,7 @@ pub struct Debugger {
     break_addr: Option<u16>,
     should_run_cpu: bool,
     run_debug: u8, //0b0000_0000 - bit 0: cpu, bit 1: human;
+    break_debug: u8, //same as run_debug
     is_step: bool,
 }
 
@@ -18,34 +19,27 @@ impl Debugger {
             should_run_cpu: false,
             run_debug: 0x00,
             is_step: false,
+            break_debug: 0x00,
         }
     }
 
     pub fn run(&mut self, instruction: &Instruction, cpu: &Cpu, mem: &Memory) {
         if self.run_debug != 0x00 {
-            let debug_cpu: bool = self.run_debug & 0b1 == 0b1;
-            let debug_human: bool = (self.run_debug >> 1) & 0b1 == 0b1;
-
-            if debug_human {
-                let v: &str = if debug_cpu { ":\n\t" } else { "\n" };
-                print!("{}{}", instruction, v);
-            }
-            if debug_cpu {
-                println!("{}", cpu);
-            }
-
+            self.print_cpu_human(self.run_debug, instruction, cpu);
             return;
-        }
-        if self.is_step {
-            println!("{}", instruction); //prints the instruction run after step.
         }
         if let Some(addr) = self.break_addr {
             if instruction.address >= addr { //>= because the provided address may point to an immediate, in which case == would never be true.
                 println!("{}", instruction);
                 self.break_addr = None;
                 self.read_loop(instruction, cpu, mem);
+            } else {
+                self.print_cpu_human(self.break_debug, instruction, cpu);
             }
         } else {
+            if self.is_step {
+                println!("{}", instruction); //prints the instruction run after step.
+            }
             self.read_loop(instruction, cpu, mem);
         }
     }
@@ -66,6 +60,19 @@ impl Debugger {
             if self.should_run_cpu {
                 break;
             }
+        }
+    }
+
+    fn print_cpu_human(&self, mask: u8, instruction: &Instruction, cpu: &Cpu) {
+        let debug_cpu: bool = mask & 0b1 == 0b1;
+        let debug_human: bool = (mask >> 1) & 0b1 == 0b1;
+
+        if debug_human {
+            let v: &str = if debug_cpu { ":\n\t" } else { "\n" };
+            print!("{}{}", instruction, v);
+        }
+        if debug_cpu {
+            println!("{}", cpu);
         }
     }
 
@@ -90,7 +97,7 @@ impl Debugger {
                     self.should_run_cpu = true;
                 },
                 "help" => {
-                    Debugger::display_help();
+                    Debugger::display_help("");
                 },
                 "run" => {
                     words.remove(0);
@@ -100,18 +107,21 @@ impl Debugger {
                     //does nothing
                 },
                 _ => {
-                    println!("Invalid command: {}", words[0]);
-                    Debugger::display_help();
+                    Debugger::display_help(&format!("Invalid command: {}", words[0]));
                 },
             }
         }
     }
 
-    fn display_help() {
+    fn display_help(error_msg: &str) {
+        if error_msg != "" {
+            println!("**ERROR: {}", error_msg);
+        }
         println!("- show [cpu|ioregs|memory]\n\tShow state of component.");
         println!("- step\n\tRun instruction pointed by PC and print it.");
         println!("- last\n\tPrint last instruction.");
-        println!("- break <address in hex>\n\tRun instructions until the instruction at the provided address is run.");
+        println!("- break <address in hex> [cpu|human]\n\tRun instructions until the instruction at the provided address is run.\
+                 \n\tIf cpu or human (or both) are set, prints each instruction run.");
         println!("- run [debug [cpu|human]]\n\tDisable the debugger and run the code.\
                              \n\tIf debug is set, information about cpu state or instruction (human friendly) or both (if both are set) will be print.");
         println!("- help\n\tShow this.");
@@ -119,46 +129,51 @@ impl Debugger {
 
     fn parse_run(&mut self, parameters: &[&str]) {
         if parameters.is_empty() || parameters.len() < 2 || parameters.len() > 3 {
-            println!("Invalid number of parameters for run.");
-            Debugger::display_help();
+            Debugger::display_help(&format!("Invalid number of parameters for run."));
         } else { //1 <= parameters <= 3
             match parameters[0] {
                 "debug" => {
-                    let mut cpu: bool = false;
-                    let mut human: bool = false;
-                    for param in &parameters[1..] {
-                        match *param {
-                            "cpu" => {
-                                cpu = true;
-                            },
-                            "human" => {
-                                human = true;
-                            },
-                            _ => {
-                                println!("Invalid parameter for `run debug`: {}", param);
-                                Debugger::display_help();
-                                break;
-                            }
-                        }
-                    }
-                    if cpu || human {
-                        self.run_debug = if human { 0b10 } else { 0b00 };
-                        self.run_debug = if cpu { self.run_debug | 0b01 } else { self.run_debug };
+                    if let Some(value) = Debugger::cpu_human_in_params(&parameters[1..]) {
+                        self.run_debug = value;
                         self.should_run_cpu = true;
                     }
                 },
                 _ => {
-                    println!("Invalid parameter for run: {}", parameters[0]);
-                    Debugger::display_help();
+                    Debugger::display_help(&format!("Invalid parameter for run: {}", parameters[0]));
                 },
             }
         }
     }
 
+    fn cpu_human_in_params(params: &[&str]) -> Option<u8> {
+        let mut cpu: bool = false;
+        let mut human: bool = false;
+        for param in params {
+            match *param {
+                "cpu" => {
+                    cpu = true;
+                },
+                "human" => {
+                    human = true;
+                },
+                _ => {
+                    Debugger::display_help(&format!("***ERROR: Invalid parameter: {}\n", param));
+                    return None;
+                }
+            }
+        }
+        let mut res: u8 = 0;
+        if cpu || human {
+            res = if human { 0b10 } else { 0b00 };
+            res = if cpu { res | 0b01 } else { res };
+        }
+
+        Some(res)
+    }
+
     fn parse_show(parameters: &[&str], cpu: &Cpu, mem: &Memory) {
         if parameters.len() != 1 {
-            println!("Invalid number of arguments for 'show'");
-            Debugger::display_help();
+            Debugger::display_help(&format!("***ERROR: Invalid number of arguments for 'show'\n"));
         } else {
             match parameters[0] {
                 "cpu" => {
@@ -172,35 +187,43 @@ impl Debugger {
                     let if_: u8 = mem.read_byte(interrupt::IF_REGISTER_ADDR);
                     let ie: u8 = mem.read_byte(interrupt::IE_REGISTER_ADDR);
 
-                    println!("IF: {:#x} {:#b}", if_, if_); 
-                    println!("IE: {:#x} {:#b}", ie, ie); 
-                    println!("TIMA: {:#x} {:#b}", tima, tima); 
+                    println!("IF: {:#x} {:#b}", if_, if_);
+                    println!("IE: {:#x} {:#b}", ie, ie);
+                    println!("TIMA: {:#x} {:#b}", tima, tima);
                     println!("TMA: {:#x} {:#b}", tma, tma);
                     println!("TAC: {:#x} {:#b}", tac, tac);
-                    println!("DIV: {:#x} {:#b}", div, div); 
+                    println!("DIV: {:#x} {:#b}", div, div);
                 },
                 "memory" => {
                     println!("{}", mem);
                 },
                 _ => {
-                    println!("Invalid parameter for 'show': {}", parameters[0]);
-                    Debugger::display_help();
+                    Debugger::display_help(&format!("***ERROR: Invalid parameter for 'show': {}\n",parameters[0]));
                 },
             }
         }
     }
 
     fn parse_break(&mut self, parameters: &[&str]) {
-        if parameters.len() != 1 {
-            println!("Invalid number of arguments for 'break'");
+        if parameters.is_empty() || parameters.len() > 3 {
+            Debugger::display_help(&format!("***ERROR: Invalid number of arguments for 'break'\n"));
         } else {
             self.break_addr = match u16::from_str_radix(&parameters[0][2..], 16) {
                 Ok(value) => Some(value),
                 Err(value) => {
-                    println!("Address is not a valid hex number: {}", value);
+                    Debugger::display_help(&format!("***ERROR: Address is not a valid hex number: {}\n", value));
                     None
                 },
             };
+            if parameters.len() >= 2 {
+                if let Some(value) = Debugger::cpu_human_in_params(&parameters[1..]) {
+                    self.break_debug = value;
+                    self.should_run_cpu = true;
+                } else {
+                    //user input incorret value
+                    self.break_addr = None;
+                }
+            }
         }
     }
 }
