618ebabf9090ad5579feb0ff2de8f15b117da559
Bruno Romero de Azevedo <brunodea@inf.ufsm.br>
2016-02-15 16:10:38 -0200

    Add command  in the Debugger.

--- src/debugger.rs
+++ src/debugger.rs
@@ -4,28 +4,32 @@ use std::io::{self, Write};
 
 pub struct Debugger {
     break_addr: Option<u16>,
-    step: bool,
     debugging: bool,
     should_run_cpu: bool,
+    is_run_debug: bool,
+    is_step: bool,
 }
 
 impl Debugger {
     pub fn new() -> Debugger {
         Debugger {
             break_addr: None,
-            step: false,
             debugging: true,
             should_run_cpu: false,
+            is_run_debug: false,
+            is_step: false,
         }
     }
 
     pub fn run(&mut self, instruction: &Instruction, cpu: &Cpu, mem: &Memory) {
         if !self.debugging {
             return;
+        } else if self.is_run_debug {
+            println!("{}", cpu);
+            return;
         }
-        if self.step {
-            println!("{}", instruction);
-            self.step = false;
+        if self.is_step {
+            println!("{}", instruction); //prints the instruction run after step.
         }
         if let Some(addr) = self.break_addr {
             if instruction.address == addr {
@@ -34,13 +38,15 @@ impl Debugger {
             }
         } else {
             loop {
+                self.should_run_cpu = false;
+                self.is_step = false;
                 print!("gbm> "); //gbm: gebemula
                 io::stdout().flush();
                 let mut input = String::new();
                 match io::stdin().read_line(&mut input) {
                     Ok(_) => {
                         input.pop(); //removes the '\n'.
-                        self.parse(&input, cpu, mem);
+                        self.parse(&input, instruction, cpu, mem);
                     },
                     Err(error) => println!("error: {}", error),
                 }
@@ -51,7 +57,7 @@ impl Debugger {
         }
     }
 
-    fn parse(&mut self, command: &str, cpu: &Cpu, mem: &Memory) {
+    fn parse(&mut self, command: &str, instruction: &Instruction, cpu: &Cpu, mem: &Memory) {
         let words: &mut Vec<&str> = &mut command.split(" ").collect();
         if !words.is_empty() {
             match words[0] {
@@ -60,8 +66,11 @@ impl Debugger {
                     Debugger::parse_show(words, cpu, mem);
                 },
                 "step" => {
-                    self.step = true;
+                    self.is_step = true;
                     self.should_run_cpu = true;
+                },
+                "last" => {
+                    println!("{}", instruction);
                 },
                 "break" => {
                     words.remove(0);
@@ -69,20 +78,38 @@ impl Debugger {
                     self.should_run_cpu = true;
                 },
                 "help" => {
-                    println!("- show [cpu|ioregs|memory]");
-                    println!("- step");
-                    println!("- break <address in hex>");
-                    println!("- run");
-                    println!("- help");
+                    println!("- show [cpu|ioregs|memory]\n\tShows state of component.");
+                    println!("- step\n\tRuns instruction pointed by PC prints it.");
+                    println!("- last\n\tPrints last instruction.");
+                    println!("- break <address in hex>\n\tRuns instructions until provided address.");
+                    println!("- run [debug]\n\tDisables the debugger and runs the code. If 'debug' is set, the cpu state after every instruction will be printed.");
+                    println!("- help\n\tShows this.");
                 },
                 "run" => {
-                    self.debugging = false;
-                    self.should_run_cpu = true;
+                    words.remove(0);
+                    self.parse_run(words);
                 },
                 "" => {
                     //does nothing
                 },
                 _ => println!("Invalid command: {}", words[0]),
+            }
+        }
+    }
+
+    fn parse_run(&mut self, parameters: &[&str]) {
+        if parameters.is_empty() {
+            self.debugging = false;
+            self.should_run_cpu = true;
+        } else if parameters.len() > 1 {
+            println!("Invalid number of parameters for run.");
+        } else {
+            match parameters[0] {
+                "debug" => {
+                    self.is_run_debug = true;
+                    self.should_run_cpu = true;
+                },
+                _ => println!("Invalid parameter for run: {}", parameters[0]),
             }
         }
     }
