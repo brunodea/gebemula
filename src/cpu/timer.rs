00e7640bddcded19469114a90313915e6d512b6f
Bruno Romero de Azevedo <brunodea@inf.ufsm.br>
2016-02-15 23:10:57 -0200

    Create struct Event for managing timed events.

--- src/cpu/timer.rs
+++ src/cpu/timer.rs
@@ -4,44 +4,79 @@ use cpu::consts;
 use std::thread;
 use std::time;
 
+struct Event {
+    cycles_counter: u32, 
+    cycles_rate: u32, //rate at which the event should happen
+    cycles_duration: Option<u32>, //duration of the event, that is, number of cycles until the cycles counter starts again.
+    cycles_duration_counter: u32,
+    on_event: bool,
+}
+
+impl Event {
+    pub fn new(cycles_rate: u32, cycles_duration: Option<u32>) -> Event {
+        Event {
+            cycles_counter: 0,
+            cycles_rate: cycles_rate,
+            cycles_duration: cycles_duration,
+            cycles_duration_counter: 0,
+            on_event: false,
+        }
+    }
+    
+    //return true if event happened.
+    pub fn update(&mut self, cycles: u32) -> bool {
+        if let Some(duration) = self.cycles_duration {
+            if self.on_event {
+                self.cycles_duration_counter += cycles;
+                if self.cycles_duration_counter >= duration {
+                    self.cycles_duration_counter = 0;
+                    self.on_event = false;
+                }
+            }
+        } else if !self.on_event {
+            self.cycles_counter += cycles;
+            if self.cycles_counter >= self.cycles_rate {
+                self.cycles_counter = 0;
+                self.on_event = true;
+                return true;
+            }
+        }
+        false
+    }
+}
+
 pub struct Timer {
-    div_cycles_counter: u32,
-    tima_cycles_counter: u32,
-    tima_rate_cycles: u32,
-    vblank_interrupt_cycles_counter: u32,
-    frame_rate_cycles: u32,
-    frame_rate_cycles_counter: u32,
+    div_event: Event,
+    tima_event: Event,
+    vblank_interrupt_event: Event,
+    frame_rate_event: Event,
     timer_started: bool,
 }
 
 impl Timer {
     pub fn new() -> Timer {
+        //TODO fix durations.
         Timer {
-            div_cycles_counter: 0,
-            tima_cycles_counter: 0,
-            tima_rate_cycles: 0,
+            div_event: Event::new(consts::DIV_REGISTER_UPDATE_RATE_CYCLES, None),
+            tima_event: Event::new(0, None),
+            vblank_interrupt_event: Event::new(consts::VBLANK_INTERRUPT_RATE_CYCLES, Some(consts::VBLANK_DURATION_CYCLES)),
+            frame_rate_event: Event::new(cycles_from_hz(60), None),
             timer_started: false,
-            vblank_interrupt_cycles_counter: 0,
-            frame_rate_cycles_counter: 0,
-            frame_rate_cycles: cycles_from_hz(60), //default: 60hz
         }
     }
 
     pub fn update(&mut self, cycles: u32, memory: &mut mem::Memory) {
-        self.div_cycles_counter += cycles;
-        if self.div_cycles_counter >= consts::DIV_REGISTER_UPDATE_RATE_CYCLES {
+        if self.div_event.update(cycles) {
             let div: u8 = memory.read_byte(consts::DIV_REGISTER_ADDR);
             memory.write_byte(consts::DIV_REGISTER_ADDR, div.wrapping_add(1));
-            self.div_cycles_counter = 0;
         }
 
         if !timer_stop(memory) {
             if !self.timer_started {
-                self.tima_rate_cycles = cycles_from_hz(input_clock(memory));
+                self.tima_event.cycles_rate = cycles_from_hz(input_clock(memory));
                 self.timer_started = true;
             }
-            self.tima_cycles_counter += cycles;
-            if self.tima_cycles_counter >= self.tima_rate_cycles {
+            if self.tima_event.update(cycles) {
                 let mut tima: u8 = memory.read_byte(consts::TIMA_REGISTER_ADDR);
                 if tima == 0xFF {
                     //overflows
@@ -51,23 +86,18 @@ impl Timer {
                 }
                 memory.write_byte(consts::TIMA_REGISTER_ADDR, tima);
                 interrupt::request(interrupt::Interrupt::TimerOverflow, memory);
-                self.tima_cycles_counter = 0;
             }
         } else {
             self.timer_started = false;
         }
 
-        self.vblank_interrupt_cycles_counter += cycles;
-        if self.vblank_interrupt_cycles_counter >= consts::VBLANK_INTERRUPT_RATE_CYCLES {
+        if self.vblank_interrupt_event.update(cycles) {
             interrupt::request(interrupt::Interrupt::VBlank, memory);
-            self.vblank_interrupt_cycles_counter = 0;
         }
 
-        self.frame_rate_cycles_counter += cycles;
-        if self.frame_rate_cycles_counter >= self.frame_rate_cycles {
+        if self.frame_rate_event.update(cycles) {
             //TODO adjust duration to consider elapsed time since the last frame.
             thread::sleep(time::Duration::new(0, 16666666)); //~1/60 seconds
-            self.frame_rate_cycles_counter = 0;
         }
     }
 }
