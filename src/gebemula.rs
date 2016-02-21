db18f423726c6b4fe728283b686e7a9b4585d150
Bruno Romero de Azevedo <brunodea@inf.ufsm.br>
2016-02-21 11:32:15 -0300

    Remove unused imports.

--- src/gebemula.rs
+++ src/gebemula.rs
@@ -10,8 +10,6 @@ use sdl2;
 use sdl2::pixels::{PixelFormatEnum, Color};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::surface::Surface;
-use sdl2::render::Renderer;
 
 pub struct Gebemula {
     cpu: Cpu,
