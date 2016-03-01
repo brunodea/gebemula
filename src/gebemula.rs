ccac6f3e179b5159ddc2309070994cb385b80530
Bruno Romero de Azevedo <brunodea@inf.ufsm.br>
2016-03-01 16:22:58 -0300

    Move graphic related stuff from Gebemula to its own struct on graphics.rs

--- src/gebemula.rs
+++ src/gebemula.rs
@@ -1,9 +1,8 @@
-use cpu::ioregister;
-use cpu::lcd::ScreenRefreshEvent;
 use cpu::cpu::{Cpu, Instruction};
 use cpu::timer::Timer;
 
 use graphics;
+use graphics::graphics::Graphics;
 
 use mem::mem::Memory;
 use debugger::Debugger;
@@ -21,13 +20,9 @@ pub struct Gebemula {
     mem: Memory,
     timer: Timer,
     debugger: Debugger,
-    screen_refresh_event: ScreenRefreshEvent,
-    screen_buffer: [u8; 160*144*4],
     game_rom: Vec<u8>,
     cycles_per_sec: u32,
-    show_bg: bool,
-    show_wn: bool,
-    show_sprites: bool,
+    graphics: Graphics,
 }
 
 impl Gebemula {
@@ -37,13 +32,9 @@ impl Gebemula {
             mem: Memory::new(),
             timer: Timer::new(),
             debugger: Debugger::new(),
-            screen_refresh_event: ScreenRefreshEvent::new(),
-            screen_buffer: [255; 160*144*4],
             game_rom: Vec::new(),
             cycles_per_sec: 0,
-            show_bg: true,
-            show_wn: true,
-            show_sprites: true,
+            graphics: Graphics::new(),
         }
     }
 
@@ -71,20 +62,7 @@ impl Gebemula {
         if cfg!(debug_assertions) {
             self.debugger.run(instruction, &self.cpu, &self.mem, &self.timer);
         }
-        if ioregister::LCDCRegister::is_lcd_display_enable(&self.mem) {
-            self.screen_refresh_event.update(instruction.cycles, &mut self.mem);
-            let bg_on: bool = ioregister::LCDCRegister::is_bg_window_display_on(&self.mem)
-                && self.show_bg;
-            let wn_on: bool = ioregister::LCDCRegister::is_window_display_on(&self.mem)
-                && self.show_wn;
-            if self.screen_refresh_event.is_scan_line {
-                graphics::graphics::update_line_buffer(bg_on, wn_on, &mut self.screen_buffer, &self.mem);
-            }
-            if ioregister::LCDCRegister::is_sprite_display_on(&self.mem) && self.show_sprites &&
-                self.screen_refresh_event.is_scan_line {
-                graphics::graphics::draw_sprites(&mut self.screen_buffer, &self.mem);
-            }
-        }
+        self.graphics.update(instruction.cycles, &mut self.mem);
         self.timer.update(instruction.cycles, &mut self.mem);
         //Checks for interrupt requests should be made after *every* instruction is
         //run.
@@ -125,19 +103,13 @@ impl Gebemula {
             for event in event_pump.poll_iter() {
                 match event {
                     Event::KeyDown { keycode: Some(Keycode::F1), .. } => {
-                        self.show_bg = !self.show_bg;
-                        self.screen_buffer = [255; 160*144*4];
-                        println!("bg: {}", self.show_bg);
+                        self.graphics.toggle_bg();
                     },
                     Event::KeyDown { keycode: Some(Keycode::F2), .. } => {
-                        self.show_wn = !self.show_wn;
-                        self.screen_buffer = [255; 160*144*4];
-                        println!("wn: {}", self.show_wn);
+                        self.graphics.toggle_wn();
                     },
                     Event::KeyDown { keycode: Some(Keycode::F3), .. } => {
-                        self.show_sprites = !self.show_sprites;
-                        self.screen_buffer = [255; 160*144*4];
-                        println!("sprites: {}", self.show_sprites);
+                        self.graphics.toggle_sprites();
                     },
                     Event::Quit {..} |
                     Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
@@ -147,9 +119,9 @@ impl Gebemula {
                 }
             }
 
-            if self.screen_refresh_event.is_display_buffer {
+            if self.graphics.update_screen() {
                 renderer.clear();
-                texture.update(None, &self.screen_buffer, 
+                texture.update(None, &self.graphics.screen_buffer,
                                graphics::consts::DISPLAY_WIDTH_PX as usize * 4).unwrap();
                 renderer.copy(&texture, None, None);
                 renderer.present();
--- src/graphics/graphics.rs
+++ src/graphics/graphics.rs
@@ -3,207 +3,276 @@ use super::super::util::util;
 use super::super::mem::mem::Memory;
 use super::super::cpu::ioregister;
 use super::super::cpu;
+use super::super::cpu::lcd::ScreenRefreshEvent;
 
-pub fn update_line_buffer(bg_on: bool, wn_on: bool, buffer: &mut [u8; 160*144*4], memory: &Memory) {
-    let curr_line: u8 = memory.read_byte(cpu::consts::LY_REGISTER_ADDR);
-    if curr_line >= consts::DISPLAY_HEIGHT_PX {
-        return;
+pub struct Graphics {
+    bg_wn_pixel_indexes: [u8; 160*144],
+    pub screen_buffer: [u8; 160*144*4],
+    screen_refresh_event: ScreenRefreshEvent,
+    bg_on: bool,
+    wn_on: bool,
+    sprites_on: bool,
+}
+
+impl Graphics {
+    pub fn new() -> Graphics {
+        Graphics {
+            screen_buffer: [255; 160*144*4],
+            bg_wn_pixel_indexes: [0; 160*144],
+            screen_refresh_event: ScreenRefreshEvent::new(),
+            bg_on: true,
+            wn_on: true,
+            sprites_on: true,
+        }
     }
-    if !bg_on && !wn_on {
-        return;
+
+    pub fn update(&mut self, cycles: u32, memory: &mut Memory) {
+        if ioregister::LCDCRegister::is_lcd_display_enable(memory) {
+            self.screen_refresh_event.update(cycles, memory);
+            if self.screen_refresh_event.is_scan_line {
+                self.update_line_buffer(memory);
+            }
+            if self.screen_refresh_event.is_scan_line {
+                self.draw_sprites(memory);
+            }
+        }
     }
-    let scx: u8 = memory.read_byte(cpu::consts::SCX_REGISTER_ADDR);
-    let mut ypos: u16 =
-        curr_line as u16 + memory.read_byte(cpu::consts::SCY_REGISTER_ADDR) as u16;
-    let wy: u8 = memory.read_byte(cpu::consts::WY_REGISTER_ADDR);
-    let wx: i16 = memory.read_byte(cpu::consts::WX_REGISTER_ADDR) as i16 - 7;
 
-    let mut is_window: bool = false;
+    fn update_line_buffer(&mut self, memory: &Memory) {
+        let mut bg_on: bool = ioregister::LCDCRegister::is_bg_window_display_on(memory);
+        let mut wn_on: bool = ioregister::LCDCRegister::is_window_display_on(memory);
+        if !bg_on && !wn_on {
+            return;
+        }
 
-    let startx: i16 = if bg_on { 0 } else { wx };
+        bg_on = bg_on & self.bg_on;
+        wn_on = wn_on & self.wn_on;
 
-    let (tile_table_addr_pattern_0, is_tile_number_signed) =
-        if ioregister::LCDCRegister::is_tile_data_0(&memory) {
-            (consts::TILE_DATA_TABLE_0_ADDR_START + 0x800, true)
-        } else {
-            (consts::TILE_DATA_TABLE_1_ADDR_START, false)
-        };
+        let curr_line: u8 = memory.read_byte(cpu::consts::LY_REGISTER_ADDR);
+        if curr_line >= consts::DISPLAY_HEIGHT_PX {
+            return;
+        }
+        if !bg_on && !wn_on {
+            return;
+        }
+        let scx: u8 = memory.read_byte(cpu::consts::SCX_REGISTER_ADDR);
+        let mut ypos: u16 =
+            curr_line as u16 + memory.read_byte(cpu::consts::SCY_REGISTER_ADDR) as u16;
+        let wy: u8 = memory.read_byte(cpu::consts::WY_REGISTER_ADDR);
+        let wx: u8 = memory.read_byte(cpu::consts::WX_REGISTER_ADDR) - 7;
 
-    let mut tile_row: u16 = (ypos/8)*32; //TODO ypos >> 3 is faster?
-    let mut tile_line: u16 = (ypos % 8)*2;
-    let mut clear_line: bool = false;
-    for i in startx..consts::DISPLAY_WIDTH_PX as i16 {
-        if wn_on && wx < consts::DISPLAY_WIDTH_PX as i16 && i >= wx && !is_window {
-            //Display Window
-            if curr_line >= wy && wy < consts::DISPLAY_HEIGHT_PX {
-                is_window = true;
-                ypos = (curr_line - wy) as u16;
-                tile_row = (ypos/8)*32; //TODO ypos >> 3 is faster?
-                tile_line = (ypos % 8)*2;
-                clear_line = false;
+        let mut is_window: bool = false;
+
+        let startx: u8 = if bg_on { 0 } else { wx };
+
+        let (tile_table_addr_pattern_0, is_tile_number_signed) =
+            if ioregister::LCDCRegister::is_tile_data_0(&memory) {
+                (consts::TILE_DATA_TABLE_0_ADDR_START + 0x800, true)
+            } else {
+                (consts::TILE_DATA_TABLE_1_ADDR_START, false)
+            };
+
+        let mut tile_row: u16 = (ypos/8)*32; //TODO ypos >> 3 is faster?
+        let mut tile_line: u16 = (ypos % 8)*2;
+        for i in startx..consts::DISPLAY_WIDTH_PX {
+            if wn_on && wx < consts::DISPLAY_WIDTH_PX && i >= wx && !is_window {
+                //Display Window
+                if curr_line >= wy && wy < consts::DISPLAY_HEIGHT_PX {
+                    is_window = true;
+                    ypos = (curr_line - wy) as u16;
+                    tile_row = (ypos/8)*32; //TODO ypos >> 3 is faster?
+                    tile_line = (ypos % 8)*2;
+                } else if !bg_on {
+                    continue;
+                }
             } else if !bg_on {
                 continue;
             }
-        } else if !bg_on {
-            continue;
-        }
-        let xpos: u8 =
-            if !is_window {
-                scx.wrapping_add(i as u8)
-            } else {
-                (i - wx) as u8
-            };
-
-        let addr_start =
-            if !is_window {
-                if ioregister::LCDCRegister::is_bg_tile_map_display_normal(&memory) {
-                    consts::BG_NORMAL_ADDR_START
+            let xpos: u8 =
+                if !is_window {
+                    scx.wrapping_add(i)
                 } else {
-                    consts::BG_WINDOW_ADDR_START
-                }
-            } else {
-                if ioregister::LCDCRegister::is_window_tile_map_display_normal(&memory) {
-                    consts::BG_NORMAL_ADDR_START
+                    i - wx
+                };
+
+            let addr_start =
+                if !is_window {
+                    if ioregister::LCDCRegister::is_bg_tile_map_display_normal(&memory) {
+                        consts::BG_NORMAL_ADDR_START
+                    } else {
+                        consts::BG_WINDOW_ADDR_START
+                    }
                 } else {
-                    consts::BG_WINDOW_ADDR_START
-                }
-            };
+                    if ioregister::LCDCRegister::is_window_tile_map_display_normal(&memory) {
+                        consts::BG_NORMAL_ADDR_START
+                    } else {
+                        consts::BG_WINDOW_ADDR_START
+                    }
+                };
 
-        let tile_col: u16 = (xpos as u16)/8; //TODO xpos >> 3 is faster?
-        let tile_addr: u16 = addr_start + tile_row + tile_col;
-        let tile_location: u16 =
-            if is_tile_number_signed {
-                let tile_number: u16 = util::sign_extend(memory.read_byte(tile_addr));
-                if util::is_neg16(tile_number) {
-                    tile_table_addr_pattern_0 - (util::twos_complement(tile_number) * consts::TILE_SIZE_BYTES as u16)
+            let tile_col: u16 = (xpos as u16)/8; //TODO xpos >> 3 is faster?
+            let tile_addr: u16 = addr_start + tile_row + tile_col;
+            let tile_location: u16 =
+                if is_tile_number_signed {
+                    let tile_number: u16 = util::sign_extend(memory.read_byte(tile_addr));
+                    if util::is_neg16(tile_number) {
+                        tile_table_addr_pattern_0 - (util::twos_complement(tile_number) * consts::TILE_SIZE_BYTES as u16)
+                    } else {
+                        tile_table_addr_pattern_0 + (tile_number * consts::TILE_SIZE_BYTES as u16)
+                    }
                 } else {
-                    tile_table_addr_pattern_0 + (tile_number * consts::TILE_SIZE_BYTES as u16)
-                }
-            } else {
-                tile_table_addr_pattern_0 + (memory.read_byte(tile_addr) as u16 * consts::TILE_SIZE_BYTES as u16)
-            };
-        let tile_col: u8 = (xpos % 8) as u8;
-        //two bytes representing 8 pixel indexes
-        let lhs: u8 = memory.read_byte(tile_location + tile_line) >> (7 - tile_col);
-        let rhs: u8 = memory.read_byte(tile_location + tile_line + 1) >> (7 - tile_col);
-        let pixel_index: u8 =
-            ioregister::bg_window_palette(((rhs << 1) & 0b10) | (lhs & 0b01), memory);
-
-        //Apply palette
-        let (r,g,b) = match pixel_index {
-            0b00 => (255,255,255),
-            0b01 => (192,192,192),
-            0b10 => (96,96,96),
-            0b11 => (0,0,0),
-            _ => unreachable!(),
-        };
-
-        let pos: usize = (curr_line as usize * consts::DISPLAY_WIDTH_PX as usize * 4) +
-            (i as usize * 4);
-
-        buffer[pos] = r;
-        buffer[pos+1] = g;
-        buffer[pos+2] = b;
-        buffer[pos+3] = 255; //alpha
-    }
-}
-
-pub fn draw_sprites(buffer: &mut [u8; 160*144*4], memory: &Memory) {
-    let curr_line: u8 = memory.read_byte(cpu::consts::LY_REGISTER_ADDR);
-    if curr_line >= consts::DISPLAY_HEIGHT_PX {
-        return;
-    }
-    let mut index: u16 = 39 * 4;
-    while index > 0 {
-        let sprite_8_16: bool = ioregister::LCDCRegister::is_sprite_8_16_on(memory);
-        let height: u8 = if sprite_8_16 { 16 } else { 8 };
-        //TODO draw sprites based on X priority.
-        let mut y: u8 = memory.read_byte(consts::SPRITE_ATTRIBUTE_TABLE + index) - 16;
-        if (curr_line < y) || (curr_line > y + height) {
-            //outside sprite
-            index -= 4;
-            continue;
-        }
-        if y == 0 || y >= 160 {
-            index -= 4;
-            continue;
-        }
-        let mut x: u8 = memory.read_byte(consts::SPRITE_ATTRIBUTE_TABLE + index + 1) - 8;
-        if x == 0 || x > 166 {
-            index -= 4;
-            continue;
-        }
-        let tile_number: u8 = memory.read_byte(consts::SPRITE_ATTRIBUTE_TABLE + index + 2);
-
-        let mut tile_location: u16 = consts::SPRITE_PATTERN_TABLE_ADDR_START +
-            (tile_number as u16 * consts::TILE_SIZE_BYTES as u16);
-
-        let flags: u8 = memory.read_byte(consts::SPRITE_ATTRIBUTE_TABLE + index + 3);
-        let above_bg: bool = (flags >> 7) & 0b1 == 0b1;
-        let y_flip: bool = (flags >> 6) & 0b1 == 0b1;
-        let x_flip: bool = (flags >> 5) & 0b1 == 0b1;
-        let obp0: bool = (flags >> 4) & 0b1 == 0b0;
-
-        let num_bits: u8 = if sprite_8_16 { 8*16 } else { 8*8 };
-        for i in 0..num_bits {
-            let tile_col: u8 = i % 8;
-            let tile_line: u8 = i / 8;
-
-            if sprite_8_16 {
-                if tile_line < 8 {
-                    tile_location = consts::SPRITE_PATTERN_TABLE_ADDR_START +
-                        ((tile_number & 0xFE) as u16 * consts::TILE_SIZE_BYTES as u16);
-                } else {
-                    tile_location = consts::SPRITE_PATTERN_TABLE_ADDR_START +
-                        (tile_number as u16 * consts::TILE_SIZE_BYTES as u16);
-                }
-            }
-
-            //tile_line*2 because each tile uses 2 bytes per line.
-            let lhs: u8 = memory.read_byte(tile_location + (tile_line as u16 * 2)) >> (7 - tile_col);
-            let rhs: u8 = memory.read_byte(tile_location + (tile_line as u16 * 2) + 1) >> (7 - tile_col);
+                    tile_table_addr_pattern_0 + (memory.read_byte(tile_addr) as u16 * consts::TILE_SIZE_BYTES as u16)
+                };
+            let tile_col: u8 = (xpos % 8) as u8;
+            //two bytes representing 8 pixel indexes
+            let lhs: u8 = memory.read_byte(tile_location + tile_line) >> (7 - tile_col);
+            let rhs: u8 = memory.read_byte(tile_location + tile_line + 1) >> (7 - tile_col);
             let pixel_data: u8 = ((rhs << 1) & 0b10) | (lhs & 0b01);
-            if pixel_data == 0 {
-                continue;
-            }
             let pixel_index: u8 =
-                ioregister::sprite_palette(obp0, pixel_data, memory);
-            let (r,g,b,a) = match pixel_index {
-                0b00 => (255,255,255,255),
-                0b01 => (192,192,192,255),
-                0b10 => (96,96,96,255),
-                0b11 => (0,0,0,255),
+                ioregister::bg_window_palette(pixel_data, memory);
+
+            //Apply palette
+            let (r,g,b) = match pixel_index {
+                0b00 => (255,255,255),
+                0b01 => (192,192,192),
+                0b10 => (96,96,96),
+                0b11 => (0,0,0),
                 _ => unreachable!(),
             };
 
-            let mut pos: usize;
+            let pos: usize = (curr_line as usize * consts::DISPLAY_WIDTH_PX as usize * 4) +
+                (i as usize * 4);
 
-            if y_flip {
-                pos =
-                    if sprite_8_16 {
-                        (y + 15 - tile_line) as usize * consts::DISPLAY_WIDTH_PX as usize * 4
+            self.screen_buffer[pos] = r;
+            self.screen_buffer[pos+1] = g;
+            self.screen_buffer[pos+2] = b;
+            self.screen_buffer[pos+3] = 255; //alpha
+
+            self.bg_wn_pixel_indexes[(curr_line as usize * consts::DISPLAY_WIDTH_PX as usize) + i as usize] = pixel_data;
+        }
+    }
+
+    fn draw_sprites(&mut self, memory: &Memory) {
+        if !ioregister::LCDCRegister::is_sprite_display_on(memory) ||
+            !self.sprites_on {
+            return;
+        }
+
+        let curr_line: u8 = memory.read_byte(cpu::consts::LY_REGISTER_ADDR);
+        if curr_line >= consts::DISPLAY_HEIGHT_PX || !self.sprites_on {
+            return;
+        }
+        let mut index: u16 = 39 * 4;
+        while index > 0 {
+            let sprite_8_16: bool = ioregister::LCDCRegister::is_sprite_8_16_on(memory);
+            let height: u8 = if sprite_8_16 { 16 } else { 8 };
+            //TODO draw sprites based on X priority.
+            let y: u8 = memory.read_byte(consts::SPRITE_ATTRIBUTE_TABLE + index) - 16;
+            if (curr_line < y) || (curr_line > y + height) {
+                //outside sprite
+                index -= 4;
+                continue;
+            }
+            if y == 0 || y >= 160 {
+                index -= 4;
+                continue;
+            }
+            let x: u8 = memory.read_byte(consts::SPRITE_ATTRIBUTE_TABLE + index + 1) - 8;
+            if x == 0 || x > 166 {
+                index -= 4;
+                continue;
+            }
+            let tile_number: u8 = memory.read_byte(consts::SPRITE_ATTRIBUTE_TABLE + index + 2);
+
+            let mut tile_location: u16 = consts::SPRITE_PATTERN_TABLE_ADDR_START +
+                (tile_number as u16 * consts::TILE_SIZE_BYTES as u16);
+
+            let flags: u8 = memory.read_byte(consts::SPRITE_ATTRIBUTE_TABLE + index + 3);
+            let above_bg: bool = (flags >> 7) & 0b1 == 0b1;
+            let y_flip: bool = (flags >> 6) & 0b1 == 0b1;
+            let x_flip: bool = (flags >> 5) & 0b1 == 0b1;
+            let obp0: bool = (flags >> 4) & 0b1 == 0b0;
+
+            let num_bits: u8 = if sprite_8_16 { 8*16 } else { 8*8 };
+            for i in 0..num_bits {
+                let tile_col: u8 = i % 8;
+                let tile_line: u8 = i / 8;// curr_line - y;//i / 8;
+
+                if sprite_8_16 {
+                    if tile_line < 8 {
+                        tile_location = consts::SPRITE_PATTERN_TABLE_ADDR_START +
+                            ((tile_number & 0xFE) as u16 * consts::TILE_SIZE_BYTES as u16);
                     } else {
-                        (y + 7 - tile_line) as usize * consts::DISPLAY_WIDTH_PX as usize * 4
-                    };
-            } else {
-                pos = (y + tile_line) as usize * consts::DISPLAY_WIDTH_PX as usize * 4;
+                        tile_location = consts::SPRITE_PATTERN_TABLE_ADDR_START +
+                            (tile_number as u16 * consts::TILE_SIZE_BYTES as u16);
+                    }
+                }
+
+                //tile_line*2 because each tile uses 2 bytes per line.
+                let lhs: u8 = memory.read_byte(tile_location + (tile_line as u16 * 2)) >> (7 - tile_col);
+                let rhs: u8 = memory.read_byte(tile_location + (tile_line as u16 * 2) + 1) >> (7 - tile_col);
+                let pixel_data: u8 = ((rhs << 1) & 0b10) | (lhs & 0b01);
+                if pixel_data == 0 {
+                    continue;
+                }
+                let pixel_index: u8 =
+                    ioregister::sprite_palette(obp0, pixel_data, memory);
+                let (r,g,b,a) = match pixel_index {
+                    0b00 => (255,255,255,255),
+                    0b01 => (192,192,192,255),
+                    0b10 => (96,96,96,255),
+                    0b11 => (0,0,0,255),
+                    _ => unreachable!(),
+                };
+
+                let mut pos: usize;
+
+                if y_flip {
+                    pos =
+                        if sprite_8_16 {
+                            (y + 15 - tile_line) as usize * consts::DISPLAY_WIDTH_PX as usize * 4
+                        } else {
+                            (y + 7 - tile_line) as usize * consts::DISPLAY_WIDTH_PX as usize * 4
+                        };
+                } else {
+                    pos = (y + tile_line) as usize * consts::DISPLAY_WIDTH_PX as usize * 4;
+                }
+
+                if x_flip {
+                    pos += (x + 7 - tile_col) as usize * 4;
+                } else {
+                    pos += (x + tile_col) as usize * 4;
+                }
+
+                //if above_bg || self.bg_wn_pixel_indexes[pos/4] == 0 {
+                {
+                    self.screen_buffer[pos] = r;
+                    self.screen_buffer[pos+1] = g;
+                    self.screen_buffer[pos+2] = b;
+                    self.screen_buffer[pos+3] = a;
+                }
             }
 
-            if x_flip {
-                pos += (x + 7 - tile_col) as usize * 4;
-            } else {
-                pos += (x + tile_col) as usize * 4;
-            }
-
-            //if above_bg {
-            {
-                buffer[pos] = r;
-                buffer[pos+1] = g;
-                buffer[pos+2] = b;
-                buffer[pos+3] = a;
+            index -= 4;
             }
         }
 
-        index -= 4;
-    }
+        pub fn toggle_bg(&mut self) {
+            self.bg_on = !self.bg_on;
+            self.screen_buffer = [255; 160*144*4];
+            println!("bg: {}", self.bg_on);
+        }
+        pub fn toggle_wn(&mut self) {
+            self.wn_on = !self.wn_on;
+            self.screen_buffer = [255; 160*144*4];
+            println!("wn: {}", self.wn_on);
+        }
+        pub fn toggle_sprites(&mut self) {
+            self.sprites_on = !self.sprites_on;
+            self.screen_buffer = [255; 160*144*4];
+            println!("sprites: {}", self.sprites_on);
+        }
+        pub fn update_screen(&self) -> bool {
+            self.screen_refresh_event.is_display_buffer
+        }
 }
