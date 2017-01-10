bf59b57a0cc301f159dbcbf616c827de66074e94
Bruno Romero de Azevedo <brunordea@gmail.com>
2017-01-09 23:30:53 -0200

    WIP: first attempt to draw colors in BG (and sprites, but it is wrong)

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -47,7 +47,7 @@ impl Graphics {
         }
     }
 
-    fn update_line_buffer(&mut self, memory: &Memory) {
+    fn update_line_buffer(&mut self, memory: &mut Memory) {
         let mut bg_on = ioregister::LCDCRegister::is_bg_window_display_on(memory);
         let mut wn_on = ioregister::LCDCRegister::is_window_display_on(memory);
 
@@ -123,6 +123,20 @@ impl Graphics {
 
             let tile_col_bg = xpos >> 3;
             let tile_addr = addr_start + tile_row + tile_col_bg;
+
+            let mut tile_vram_bank = 0;
+            let mut attr = None;
+            if mode_color {
+                // change vbk to 1 so we can read the bg map attributes.
+                memory.write_byte(cpu::consts::VBK_REGISTER_ADDR, 1);
+
+                attr = Some(memory.read_byte(tile_addr));
+                tile_vram_bank = (attr.unwrap() >> 3) & 0b1;
+
+                // set vbk to use the correct bank for the tile data.
+                memory.write_byte(cpu::consts::VBK_REGISTER_ADDR, tile_vram_bank);
+            }
+
             let tile_location = if is_tile_number_signed {
                 let mut tile_number = util::sign_extend(memory.read_byte(tile_addr));
                 if util::is_neg16(tile_number) {
@@ -139,19 +153,44 @@ impl Graphics {
             // two bytes representing 8 pixel indexes
             let lhs = memory.read_byte(tile_location + tile_line) >> (7 - tile_col);
             let rhs = memory.read_byte(tile_location + tile_line + 1) >> (7 - tile_col);
+
             let pixel_data = ((rhs << 1) & 0b10) | (lhs & 0b01);
-            let pixel_index = ioregister::bg_window_palette(pixel_data, memory);
 
-            // Apply palette
-            let (r, g, b) = consts::DMG_PALETTE[pixel_index as usize];
-            self.bg_wn_pixel_indexes[buffer_pos] = pixel_data;
+            if let Some(attr) = attr {
+                let palette_num = attr & 0b111;
+                //TODO: flip
+                let hflip = (attr >> 5) & 0b1 == 0b1;
+                let vflip = (attr >> 6) & 0b1 == 0b1;
+                //TODO: use priority.
+                let priority = (attr >> 7) & 0b1; //0: OAM priority; 1: BG priority
 
-            let buffer_pos = buffer_pos * 4; //*4 because of RGBA
+                let palette_h = memory.read_bg_palette((palette_num * 8) + (pixel_data * 2)); //each palette uses 8 bytes.
+                let palette_l = memory.read_bg_palette((palette_num * 8) + 1 + (pixel_data * 2)); // pixel_data chooses the palette index. *2 because each color intensity uses two bytes.
 
-            self.screen_buffer[buffer_pos] = r;
-            self.screen_buffer[buffer_pos + 1] = g;
-            self.screen_buffer[buffer_pos + 2] = b;
-            self.screen_buffer[buffer_pos + 3] = 255; //alpha
+                let r = palette_l & 0b0001_1111;
+                let g = (((palette_h & 0b11) << 3) | (palette_l >> 5));
+                let b = (palette_h >> 2) & 0b11111;
+
+                let buffer_pos = buffer_pos * 4; //*4 because of RGBA
+
+                self.screen_buffer[buffer_pos] = 255 * (r / 0x1F);
+                self.screen_buffer[buffer_pos + 1] = 255 * (g / 0x1F);
+                self.screen_buffer[buffer_pos + 2] = 255 * (b / 0x1F);
+                self.screen_buffer[buffer_pos + 3] = 255; //alpha
+            } else {
+                memory.write_byte(cpu::consts::VBK_REGISTER_ADDR, 0);
+                let pixel_index = ioregister::bg_window_palette(pixel_data, memory);
+                // Apply palette
+                let (r, g, b) = consts::DMG_PALETTE[pixel_index as usize];
+                self.bg_wn_pixel_indexes[buffer_pos] = pixel_data;
+
+                let buffer_pos = buffer_pos * 4; //*4 because of RGBA
+
+                self.screen_buffer[buffer_pos] = r;
+                self.screen_buffer[buffer_pos + 1] = g;
+                self.screen_buffer[buffer_pos + 2] = b;
+                self.screen_buffer[buffer_pos + 3] = 255; //alpha
+            }
         }
     }
 
@@ -202,6 +241,8 @@ impl Graphics {
             let y_flip = (flags >> 6) & 0b1 == 0b1;
             let x_flip = (flags >> 5) & 0b1 == 0b1;
             let obp0 = (flags >> 4) & 0b1 == 0b0;
+            let tile_vram_bank = (flags >> 3) & 0b1;
+            let palette_num = flags & 0b111;
 
             x -= 8;
             let endx = if x + 8 >= consts::DISPLAY_WIDTH_PX as i16 {
@@ -220,9 +261,6 @@ impl Graphics {
                 if pixel_data == 0 {
                     continue;
                 }
-                let pixel_index = ioregister::sprite_palette(obp0, pixel_data, memory);
-                // sprite color pallete
-                let (r, g, b) = consts::DMG_PALETTE[pixel_index as usize];
 
                 let mut buffer_pos: usize;
 
@@ -246,11 +284,40 @@ impl Graphics {
                     continue;
                 }
 
-                if above_bg || self.bg_wn_pixel_indexes[buffer_pos] == 0 {
+                if mode_color {
                     buffer_pos *= 4;
                     if buffer_pos > self.screen_buffer.len() - 4 {
                         continue;
                     }
+                    //TODO: use tile_vram_bank
+                    let palette_h = memory.read_sprite_palette(palette_num * 8); //each palette uses 8 bytes.
+                    let palette_l = memory.read_sprite_palette((palette_num * 8) + 1);
+
+                    let r = palette_l & 0b0001_1111;
+                    let g = (((palette_h & 0b11) << 3) | (palette_l >> 5));
+                    let b = (palette_h >> 2) & 0b11111;
+
+                    self.screen_buffer[buffer_pos] = 255 * (r / 0x1F);
+                    self.screen_buffer[buffer_pos + 1] = 255 * (g / 0x1F);
+                    self.screen_buffer[buffer_pos + 2] = 255 * (b / 0x1F);
+                    self.screen_buffer[buffer_pos + 3] = 255; //alpha
+                } else if above_bg || self.bg_wn_pixel_indexes[buffer_pos] == 0 {
+                    buffer_pos *= 4;
+                    if buffer_pos > self.screen_buffer.len() - 4 {
+                        continue;
+                    }
+
+                    let palette_h = memory.read_bg_palette(palette_num * 8); //each palette uses 8 bytes.
+                    let palette_l = memory.read_bg_palette((palette_num * 8) + 1);
+
+                    let r = palette_l & 0b0001_1111;
+                    let g = (palette_h & 0b11) | (palette_l >> 5);
+                    let b = palette_h >> 2;
+
+                    let pixel_index = ioregister::sprite_palette(obp0, pixel_data, memory);
+                    // sprite color pallete
+                    let (r, g, b) = consts::DMG_PALETTE[pixel_index as usize];
+
                     self.screen_buffer[buffer_pos] = r;
                     self.screen_buffer[buffer_pos + 1] = g;
                     self.screen_buffer[buffer_pos + 2] = b;
